2d4f78fa2ce38d0f781453fa7ec8443a2b06d180
vungho <hoquocvung@gmail.com>
2017-09-20 00:21:24 +0700

    [New] Manager meu

--- DisksRent.DisksRentWeb/Controllers/ManagerController.cs
+++ DisksRent.DisksRentWeb/Controllers/ManagerController.cs
@@ -9,9 +9,36 @@ namespace DisksRent.DisksRentWeb.Controllers
     public class ManagerController : Controller
     {
         // GET: Manager
-        public ActionResult Index()
+
+        public ActionResult QuanLyKhachHang()
         {
             return View();
         }
+
+        public ActionResult QuanLyThueTraDia()
+        {
+            return View();
+        }
+
+      
+        public ActionResult QuanLyTieuDe()
+        {
+            return View();
+        }
+
+        public ActionResult QuanLyDatDia()
+        {
+            return View();
+        }
+
+        public ActionResult QuanLyDia()
+        {
+            return View();
+        }
+        public ActionResult QuanLyPhiTre()
+        {
+            return View();
+        }
+
     }
 }
